76579adf5d39e3fd9c1eff2d2747c629a790777f
tsorak <karost0249@gmail.com>
2023-04-11 17:45:05 +0200

    Add user registration endpoint

--- src/router.js
+++ src/router.js
@@ -1,9 +1,15 @@
-const { Router } = require("express");
+const { Router, json } = require("express");
+
+const { validate } = require("./validator");
+
+const { registerController } = require("./controllers/user/registerController");
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+	res.send("Hello World!");
 });
+
+router.post("/user", json(), validate.user.register, registerController);
 
 exports.router = router;
