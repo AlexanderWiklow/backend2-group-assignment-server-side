01e037337bcfbb34994afa0ce2fad5f4174da277
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-11 16:18:11 +0200

    test test

--- src/server.js
+++ src/server.js
@@ -4,5 +4,12 @@ const app = express();
 
 app.use(router);
 
+function add(a, b) {
+  return a + b;
+}
+
 app.listen(5050);
 console.log("http://localhost:5050");
+
+// exports.add = add;
+module.exports = add;
