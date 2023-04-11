97974e3772a0f7011ff6e44a876cc0113948402c
tsorak <karost0249@gmail.com>
2023-04-11 17:22:10 +0200

    Add functions for creating and validating JWT tokens

---
+++ src/authorisation.js
@@ -0,0 +1,15 @@
+const { sign, verify } = require("jsonwebtoken");
+require("dotenv").config();
+
+const key = process.env.JWT_SECRET || "";
+if (!key) throw new Error("Please assign a value for JWT_SECRET in a .env file in the project root");
+
+function generateToken(userID) {
+	return sign({ userID }, key, { expiresIn: "1h" });
+}
+
+function validateToken(token) {
+	return verify(token, key);
+}
+
+exports = { generateToken, validateToken };
