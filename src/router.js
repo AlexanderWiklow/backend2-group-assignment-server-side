8d038ac3bdab09cfc96ff13b0ad79df761153332
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-14 13:51:10 +0200

    add endpoint for login

--- src/router.js
+++ src/router.js
@@ -1,15 +1,16 @@
 const { Router, json } = require("express");
-
 const { validate } = require("./validator");
-
 const { registerController } = require("./controllers/user/registerController");
+const { loginController } = require("./controllers/user/loginController");
 
 const router = Router();
 
 router.get("/", (req, res) => {
-	res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 router.post("/user", json(), validate.user.register, registerController);
+
+router.post("/user/login", json(), validate.user.login, loginController);
 
 exports.router = router;
