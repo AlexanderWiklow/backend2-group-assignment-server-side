c2356c6ae6ceb21d9fece668a51685efdaaea1f6
tsorak <karost0249@gmail.com>
2023-04-14 23:44:27 +0200

    Add validation for profile endpoint

--- src/validator.js
+++ src/validator.js
@@ -4,40 +4,44 @@ const validator = require("express-joi-validation").createValidator();
 const { isValidMongoDBObjectId } = require("./joi-custom-validators/objectIdValidator.js");
 
 const schema = {
-  user: {
-    register: joi.object({
-      username: joi.string().required(),
-      password: joi.string().required()
-    }),
-    login: joi.object({
-      username: joi.string().required(),
-      password: joi.string().required()
-    })
-  },
-  post: {
-    create: joi.object({
-      content: joi.string().required()
-    }),
-    update: joi.object({
-      content: joi.string().required()
-    }),
-    like: joi.object({
-      targetUser: joi.string().required(),
-      targetPost: joi.required().custom(isValidMongoDBObjectId)
-    })
-  }
+	user: {
+		profile: joi.object({
+			username: joi.string().required()
+		}),
+		register: joi.object({
+			username: joi.string().required(),
+			password: joi.string().required()
+		}),
+		login: joi.object({
+			username: joi.string().required(),
+			password: joi.string().required()
+		})
+	},
+	post: {
+		create: joi.object({
+			content: joi.string().required()
+		}),
+		update: joi.object({
+			content: joi.string().required()
+		}),
+		like: joi.object({
+			targetUser: joi.string().required(),
+			targetPost: joi.required().custom(isValidMongoDBObjectId)
+		})
+	}
 };
 
 const validate = {
-  user: {
-    register: validator.body(schema.user.register),
-    login: validator.body(schema.user.login)
-  },
-  post: {
-    create: validator.body(schema.post.create),
-    update: validator.body(schema.post.update),
-    like: validator.body(schema.post.like)
-  }
+	user: {
+		profile: validator.params(schema.user.profile),
+		register: validator.body(schema.user.register),
+		login: validator.body(schema.user.login)
+	},
+	post: {
+		create: validator.body(schema.post.create),
+		update: validator.body(schema.post.update),
+		like: validator.body(schema.post.like)
+	}
 };
 
 exports.validate = validate;
