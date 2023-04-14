a956b16c74d2d8ff944bd45c63ff537d251fbc79
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-14 13:52:23 +0200

    add login with username and password to schema and to validate

--- src/validator.js
+++ src/validator.js
@@ -2,18 +2,23 @@ const joi = require("joi");
 const validator = require("express-joi-validation").createValidator();
 
 const schema = {
-	user: {
-		register: joi.object({
-			username: joi.string().required(),
-			password: joi.string().required()
-		})
-	}
+  user: {
+    register: joi.object({
+      username: joi.string().required(),
+      password: joi.string().required(),
+    }),
+    login: joi.object({
+      username: joi.string().required(),
+      password: joi.string().required(),
+    }),
+  },
 };
 
 const validate = {
-	user: {
-		register: validator.body(schema.user.register)
-	}
+  user: {
+    register: validator.body(schema.user.register),
+    login: validator.body(schema.user.login),
+  },
 };
 
 exports.validate = validate;
