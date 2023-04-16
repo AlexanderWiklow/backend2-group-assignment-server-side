b4ce700f357ebf8c39bbe34e7330744fb4a38cda
tsorak <karost0249@gmail.com>
2023-04-16 21:05:37 +0200

    Add post like endpoint

--- src/router.js
+++ src/router.js
@@ -1,27 +1,28 @@
 const { Router, json } = require("express");
+
 const { validate } = require("./validator");
+const { verifySessionMiddleware } = require("./session");
+
 const { registerController } = require("./controllers/user/registerController");
 const { loginController } = require("./controllers/user/loginController");
-const {
-  postCreateController,
-} = require("./controllers/post/postCreateController");
-const { verifySessionMiddleware } = require("./session");
+
+const { postCreateController } = require("./controllers/post/postCreateController");
+const { postLikeController } = require("./controllers/post/postLikeController");
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+	res.send("Hello World!");
 });
 
+//user
 router.post("/user", json(), validate.user.register, registerController);
 
 router.post("/user/login", json(), validate.user.login, loginController);
-router.post(
-  "/post",
-  json(),
-  validate.post.create,
-  verifySessionMiddleware,
-  postCreateController
-);
+
+//post
+router.post("/post", json(), validate.post.create, verifySessionMiddleware, postCreateController);
+
+router.post("/post/like", json(), validate.post.like, verifySessionMiddleware, postLikeController);
 
 exports.router = router;
