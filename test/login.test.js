4f1e165ac5a4daee79e9afc276d29f933e89ae7a
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-17 10:35:33 +0200

    create tests for two scenarios, one for successful- and one for failed login

--- test/login.test.js
+++ test/login.test.js
@@ -1,5 +1,33 @@
 const request = require("supertest");
+const { router } = require("../src/router.js");
+const express = require("express");
+const app = express();
 
-const database = require("../src/database.js");
+app.use(router);
 
-const { app } = require("../src/server.js");
+// two scenarios for login testing - one for success and one for failure
+describe("POST /user/login", () => {
+  test("should return 200, and give an accessToken", async () => {
+    const response = await request(app)
+      .post("/user/login")
+      .send({ username: "Alex", password: "123456" })
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+
+    const cookie = response.headers["set-cookie"][0];
+    expect(cookie).toMatch(/accessToken=/);
+  });
+
+  test("should return 400, and not give an accessToken", async () => {
+    const response = await request(app)
+      .post("/user/login")
+      .send({ username: "", password: "" })
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(400);
+
+    const cookie = response.headers["set-cookie"];
+    expect(cookie).toBeUndefined();
+  });
+});
