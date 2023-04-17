effb5c72442c07d5ac0c2b9189b989e4ea575d62
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-17 14:59:57 +0200

    import and use cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const { router } = require("./router.js");
+const cors = require("cors");
 const app = express();
 
 app.use(router);
+
+// Use the cors middleware to allow the client to access the server
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.listen(5050);
 console.log("http://localhost:5050");
