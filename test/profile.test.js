d8b870ea24fbc53c27cf23e5f36683a710006bde
tsorak <karost0249@gmail.com>
2023-04-17 11:36:20 +0200

    Add clientHasLiked tests
    Add tests for checking weather a user has liked and not yet liked a post

--- test/profile.test.js
+++ test/profile.test.js
@@ -1,6 +1,9 @@
 const request = require("supertest");
+const cookie = require("cookie");
 
 const { router } = require("../src/router.js");
+const { createSession } = require("../src/session.js");
+const database = require("../src/database.js");
 
 const express = require("express");
 const app = express();
@@ -17,6 +20,40 @@ describe("GET /profile/:username", () => {
 		expect(user).toHaveProperty("id");
 		expect(user).toHaveProperty("username");
 		expect(user).toHaveProperty("posts");
+	});
+
+	test("should have clientHasLiked set to true", async () => {
+		const db = await database.getConnection();
+		const user = await db.collection("users").findOne({ username: "Tor" });
+		const userID = user._id.toString();
+		const token = createSession(userID);
+
+		const clientCookie = cookie.serialize("accessToken", token.accessToken, token.cookieConfig);
+
+		const response = await request(app).get("/profile/Tor").set("Accept", "application/json").set("Cookie", clientCookie);
+
+		const foundProfile = response.body;
+		console.log(foundProfile);
+
+		expect(foundProfile).toHaveProperty("posts");
+		expect(foundProfile.posts[0].clientHasLiked).toBe(true);
+	});
+
+	test("should have clientHasLiked set to false", async () => {
+		const db = await database.getConnection();
+		const user = await db.collection("users").findOne({ username: "Karl" });
+		const userID = user._id.toString();
+		const token = createSession(userID);
+
+		const clientCookie = cookie.serialize("accessToken", token.accessToken, token.cookieConfig);
+
+		const response = await request(app).get("/profile/Tor").set("Accept", "application/json").set("Cookie", clientCookie);
+
+		const foundProfile = response.body;
+		console.log(foundProfile);
+
+		expect(foundProfile).toHaveProperty("posts");
+		expect(foundProfile.posts[0].clientHasLiked).toBe(false);
 	});
 
 	test("should return 404 as the user was not found", async () => {
