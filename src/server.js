24f4e32c77772d00e6cc0f5b75b168fe02cfe02c
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-18 09:28:29 +0200

    move app.use(router) below cors

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ const { router } = require("./router.js");
 const cors = require("cors");
 const app = express();
 
-app.use(router);
-
 // Use the cors middleware to allow the client to access the server
 app.use(
   cors({
@@ -12,6 +10,8 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(router);
 
 app.listen(5050);
 console.log("http://localhost:5050");
