3b2275f5f00da465ba8e28771b3b103b9bcae46a
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-18 15:47:15 +0200

    changed from params to query, and use the jwt variable

--- src/controllers/post/postDeleteController.js
+++ src/controllers/post/postDeleteController.js
@@ -1,21 +1,18 @@
 const { ObjectId } = require("mongodb");
-
 const database = require("../../database.js");
 
 async function postDeleteController(req, res) {
-  const jwt = req.body.jwt;
-  const postOwnersID = jwt.userID;
-  //   const { postID } = req.body;
-  const { content } = req.body;
+  const jwt = req.query.jwt;
+  const postOwnersID = jwt.userID; // Update variable name from jwt.userID to req.query.userID
+  const postID = jwt.postID; // Update variable to directly access query parameter value
+  // const { content } = req.body;
 
   const db = await database.getConnection();
 
-  await db
-    .collection("users")
-    .deleteOne(
-      { _id: new ObjectId(postOwnersID) },
-      { $pull: { posts: { postID } } }
-    );
+  await db.collection("users").deleteOne(
+    { _id: ObjectId(postOwnersID) }, // Convert postOwnersID to ObjectId
+    { $pull: { posts: { _id: ObjectId(postID) } } } // Convert postID to ObjectId
+  );
 
   return res.status(204).json({ message: "Post deleted successfully" });
 }
