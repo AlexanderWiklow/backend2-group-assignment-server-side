99092494f2742fe4790a7960933d6022e6a8a784
June Zhao <110632376+juneyunyun@users.noreply.github.com>
2023-04-18 16:23:09 +0200

    add post update endpoint

--- src/router.js
+++ src/router.js
@@ -7,12 +7,13 @@ const { registerController } = require("./controllers/user/registerController");
 const { loginController } = require("./controllers/user/loginController");
 
 const { postCreateController } = require("./controllers/post/postCreateController");
+const { postUpdateController } = require("./controllers/post/postUpdateController");
 const { postLikeController } = require("./controllers/post/postLikeController");
 
 const router = Router();
 
 router.get("/", (req, res) => {
-	res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 //user
@@ -24,5 +25,7 @@ router.post("/user/login", json(), validate.user.login, loginController);
 router.post("/post", json(), validate.post.create, verifySessionMiddleware, postCreateController);
 
 router.post("/post/like", json(), validate.post.like, verifySessionMiddleware, postLikeController);
+
+router.put("/post/:postID", json(), validate.post.update, verifySessionMiddleware, postUpdateController);
 
 exports.router = router;
