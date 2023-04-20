aa6521f1121678ed975ef9c0c67d0203995fa18c
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-20 17:51:06 +0200

    add updateOne

--- src/controllers/post/postDeleteController.js
+++ src/controllers/post/postDeleteController.js
@@ -2,18 +2,23 @@ const { ObjectId } = require("mongodb");
 const database = require("../../database.js");
 
 async function postDeleteController(req, res) {
-  const jwt = req.query.jwt;
-  const postOwnersID = jwt.userID; // Update variable name from jwt.userID to req.query.userID
-  const postID = jwt.postID; // Update variable to directly access query parameter value
+  // const jwt = req.query.jwt;
+  const postOwnersID = req.query.userID;
+  const postID = req.query.postID; // Update variable to directly access query parameter value
   // const { content } = req.body;
+  console.log("Hello");
+
+  console.log("postOwnersID", postOwnersID);
+  console.log("postID", postID);
 
   const db = await database.getConnection();
 
-  await db.collection("users").deleteOne(
-    { _id: ObjectId(postOwnersID) }, // Convert postOwnersID to ObjectId
-    { $pull: { posts: { _id: ObjectId(postID) } } } // Convert postID to ObjectId
-  );
-
+  await db
+    .collection("users")
+    .updateOne(
+      { _id: new ObjectId(postOwnersID), "posts._id": new ObjectId(postID) },
+      { $pull: { posts: { _id: new ObjectId(postID) } } }
+    );
   return res.status(204).json({ message: "Post deleted successfully" });
 }
 
