7a3c3540ec507c2424244d124734fa94fbebfcbb
June Zhao <110632376+juneyunyun@users.noreply.github.com>
2023-04-20 17:17:57 +0200

    add post comment controller

---
+++ src/controllers/post/postCommentController.js
@@ -0,0 +1,28 @@
+const { ObjectId } = require("mongodb");
+const database = require("../../database.js");
+
+async function postCommentController(req, res) {
+  const jwt = req.body.jwt;
+  const commenterID = jwt.userID;
+  const { postId } = req.params;
+  const { comment } = req.body;
+
+  const db = await database.getConnection();
+  const newComment = {
+    sender: new ObjectId(commenterID),
+    comment,
+    createdAt: new Date()
+  };
+
+  const result = await db
+    .collection("users")
+    .updateOne({ "posts._id": new ObjectId(postId) }, { $push: { "posts.$.comments": newComment } });
+
+  if (result.modifiedCount === 0) {
+    return res.status(404).json({ message: "Post not found or comment not added" });
+  }
+
+  return res.status(201).json({ message: "Comment added successfully" });
+}
+
+module.exports = { postCommentController };
