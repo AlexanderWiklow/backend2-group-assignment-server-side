60ad40141cd52069f48f69b7e7025fc367251053
June Zhao <110632376+juneyunyun@users.noreply.github.com>
2023-04-20 10:05:03 +0200

    remove postId validation, fix objectId

--- src/controllers/post/postUpdateController.js
+++ src/controllers/post/postUpdateController.js
@@ -8,16 +8,14 @@ async function postUpdateController(req, res) {
   const { postId } = req.params;
   const { content } = req.body;
 
-  if (!ObjectId.isValid(postId)) {
-    return res.status(400).json({ message: "Invalid post ID" });
-  }
-
   const db = await database.getConnection();
   const updatedPost = {
     "posts.$.content": content
   };
 
-  const result = await db.collection("users").updateOne({ _id: ObjectId(postOwnersID), "posts._id": ObjectId(postId) }, { $set: updatedPost });
+  const result = await db
+    .collection("users")
+    .updateOne({ _id: new ObjectId(postOwnersID), "posts._id": new ObjectId(postId) }, { $set: updatedPost });
 
   if (result.modifiedCount === 0) {
     return res.status(404).json({ message: "Post not found or not updated" });
