56fda4be3439ec00c13febb3ce2193a2121cd433
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-20 19:00:57 +0200

    remove unused code

--- src/controllers/post/postDeleteController.js
+++ src/controllers/post/postDeleteController.js
@@ -2,14 +2,8 @@ const { ObjectId } = require("mongodb");
 const database = require("../../database.js");
 
 async function postDeleteController(req, res) {
-  // const jwt = req.query.jwt;
   const postOwnersID = req.query.userID;
-  const postID = req.query.postID; // Update variable to directly access query parameter value
-  // const { content } = req.body;
-  console.log("Hello");
-
-  console.log("postOwnersID", postOwnersID);
-  console.log("postID", postID);
+  const postID = req.query.postID;
 
   const db = await database.getConnection();
 
