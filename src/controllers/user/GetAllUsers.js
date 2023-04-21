253376701a62966e857c62a40fd5ab89e7702301
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-21 14:48:45 +0200

    fetch all data from users except their password field

--- src/controllers/user/GetAllUsers.js
+++ src/controllers/user/GetAllUsers.js
@@ -1,12 +1,22 @@
-// get all users from mongodb
 const database = require("../../database.js");
 
+// fetch all users from the database except for their password field. This is done by using the projection option in the find() method.
 async function getAllUsers(req, res) {
-  const db = await database.getConnection();
+  try {
+    const db = await database.getConnection();
 
-  const users = await db.collection("users").find().toArray();
+    const users = await db
+      .collection("users")
+      .find({}, { projection: { password: 0 } })
+      .toArray();
 
-  return res.status(200).json(users);
+    console.log("users: ", users);
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 module.exports = { getAllUsers };
