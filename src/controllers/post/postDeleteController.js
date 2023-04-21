a5f3e4cfb899f153d5bc997f8f785ff431e966ee
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-21 10:40:42 +0200

    get userID from cookie, add try catch block and more error handling

--- src/controllers/post/postDeleteController.js
+++ src/controllers/post/postDeleteController.js
@@ -2,18 +2,29 @@ const { ObjectId } = require("mongodb");
 const database = require("../../database.js");
 
 async function postDeleteController(req, res) {
-  const postOwnersID = req.query.userID;
+  const jwt = req.body.jwt;
+  const postOwnersID = jwt.userID;
   const postID = req.query.postID;
 
   const db = await database.getConnection();
 
-  await db
-    .collection("users")
-    .updateOne(
-      { _id: new ObjectId(postOwnersID), "posts._id": new ObjectId(postID) },
-      { $pull: { posts: { _id: new ObjectId(postID) } } }
-    );
-  return res.status(204).json({ message: "Post deleted successfully" });
+  try {
+    const result = await db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(postOwnersID), "posts._id": new ObjectId(postID) },
+        { $pull: { posts: { _id: new ObjectId(postID) } } }
+      );
+
+    if (result.modifiedCount === 0) {
+      throw new Error("Post not found");
+    }
+
+    return res.status(204).json({ message: "Post deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 module.exports = { postDeleteController };
